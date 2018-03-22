6b73caa9792203947c2b44d996b60ee5d99de65f
Kondzik <kmaslowiec@gmail.com>
2018-03-22 12:14:40 +0100

    version 0.1.5
    * first songs, artists and genre activities added
    * event handlers added to link the activities with the TextViews

--- app/src/main/java/com/example/android/awesomemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/awesomemusicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.awesomemusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        // SONGS ON CLICK EVENT
+        TextView songs = findViewById(R.id.button_songs);
+        songs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // ARTISTS ON CLICK EVENT
+        TextView artists = findViewById(R.id.button_artists);
+        artists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ArtistsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // ARTISTS ON CLICK EVENT
+        TextView genre = findViewById(R.id.button_genre);
+        genre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, GenreActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
