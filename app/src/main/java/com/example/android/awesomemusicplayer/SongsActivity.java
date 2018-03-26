74f77771421349c7ddea182c9281f13ece62bcff
Kondzik <kmaslowiec@gmail.com>
2018-03-26 12:40:15 +0200

    version 0.1.8 END OF KNOW TERRITORY
    * SongsAdapter created.
    
    * The list of songs visible.
    
    * Adapter implemented

--- app/src/main/java/com/example/android/awesomemusicplayer/SongsActivity.java
+++ app/src/main/java/com/example/android/awesomemusicplayer/SongsActivity.java
@@ -2,6 +2,10 @@ package com.example.android.awesomemusicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SongsActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class SongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        Songs[] songsArray = {
+                new Songs("Thriller", "Micheal Jackson", "Pop"),
+                new Songs("bloody roots", "Sepultura", "Heavy Metal"),
+                new Songs("These Are The Days Of Our Lives", "Queen", "Rock"),
+                new Songs("Nie pytaj mnie", "Tomek Lipiński", "Rock"),
+                new Songs("Supergirl", "Anna Naklab feat. Alle Farben & Younotus", "Pop"),
+                new Songs("Enjoy the silence", "Depeche Mode", "Electro Pop"),
+                new Songs("Come with me", "P.Daddy", "Hip-hop"),
+                new Songs("Hurt", "Johnny Cash", "Country"),
+                new Songs("All along the watchtower", "Bob Dylan", "Rock"),
+                new Songs("More Human Than Human", "White Zombie", "Trash Metal")
+        };
+
+        ArrayList<Songs> songsArrayList = new ArrayList<>(Arrays.asList(songsArray));
+
+        SongsAdapter adapter = new SongsAdapter(this, songsArrayList);
+
+        ListView listView = (ListView)  findViewById(R.id.list_songs);
+
+        listView.setAdapter(adapter);
+
+
+
     }
 }
